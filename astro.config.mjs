8f84018b7876da59c739c341fe51105dca7e76fa
drewmcarthur <drewmcarthur1@gmail.com>
2025-01-25 20:08:09 -0700

    init for new repo

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  redirects: {
+    "/app": "//app.pmsky.social",
+    "/bsky": "//bsky.app/profile/pmsky.social",
+  },
+});
